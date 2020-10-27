1d4f26aa1e98acc31297300bf094b218cdea9a94
Kennadee <kennadee.ray@gmail.com>
2020-10-27 18:55:12 -0500

    typedefs edit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,28 +2,28 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     input saveArr {
-        authors: [String!]
+        authors: [String]
         description: String!
         title: String!
         bookId: String!
-        image: String!
-        link: String!
+        image: String
+        link: String
     }
 
     type Book {
-        bookId: String!
-        authors: [String!]
-        description: String!
+        bookId: ID!
+        authors: [String]
+        description: String
         title: String!
-        image: String!
-        link: String!
+        image: String
+        link: String
     }
 
     type User {
         _id: ID!
         username: String!
-        email: String!
-        bookCount: Int!
+        email: String
+        bookCount: Int
         savedBooks: [Book]
     }
 
@@ -37,10 +37,10 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login: Auth
-        addUser: Auth
-        addBook(input: saveArr): User
-        removeBook(id: ID!):  User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addBook(bookData: saveArr!): User
+        removeBook(bookId: ID!):  User
     }
 `;
 
